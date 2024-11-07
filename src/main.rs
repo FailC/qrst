37a13bb3fc3ccb1c17ba4dda3132dbf26952ff3a
FailC <f.nikodem@web.de>
2024-11-07 01:10:45 +0100

    add saving to svg

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use clap::Parser;
-use fast_qr::convert::{image::ImageBuilder, Builder, Shape};
+use fast_qr::convert::{image::ImageBuilder, svg::SvgBuilder, Builder, Shape};
 use fast_qr::qr::QRBuilder;
 use std::process::ExitCode;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Input for QRCode
+    /// Create QR-Code
     #[arg(short, long)]
     input: String,
 
-    /// Save QRCode to a file
+    /// Save as PNG
     #[arg(short, long)]
     file: Option<Option<String>>,
+
+    /// Save as SVG
+    #[arg(short, long)]
+    svg: Option<Option<String>>,
 }
 
-fn save_to_file(qrcode: &fast_qr::QRCode, file_name: String) {
+fn save_to_svg(qrcode: &fast_qr::QRCode, file_name: String) {
+    let _svg = SvgBuilder::default()
+        .shape(Shape::RoundedSquare)
+        .to_file(&qrcode, &file_name);
+}
+
+fn save_to_png(qrcode: &fast_qr::QRCode, file_name: String) {
     let _img = ImageBuilder::default()
         .shape(Shape::Square)
         .background_color([255, 255, 255, 255])
@@ -29,15 +39,21 @@ fn build_qr_code(args: Args) -> Result<(), String> {
         return Err(String::from("ERROR: couldn't build QR-code"));
     };
 
-    if let Some(file_name) = args.file {
-        let file_name = file_name.unwrap_or(String::from("out.png"));
-        save_to_file(&qrcode, file_name);
+    if args.file.is_some() || args.svg.is_some() {
+        if let Some(file_name) = args.file {
+            let file_name = file_name.unwrap_or(String::from("out.png"));
+            save_to_png(&qrcode, file_name);
+        }
+
+        if let Some(file_name) = args.svg {
+            let file_name = file_name.unwrap_or(String::from("out.svg"));
+            save_to_svg(&qrcode, file_name);
+        }
         return Ok(());
-    } else {
-        let string = qrcode.to_str();
-        println!("{}", string);
-        Ok(())
     }
+    let string = qrcode.to_str();
+    println!("{}", string);
+    Ok(())
 }
 
 fn main() {
