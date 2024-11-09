4daedf0d7fb967265a502407543147990e960c3c
FailC <f.nikodem@web.de>
2024-11-09 03:21:43 +0100

    check resolution input, change error in build_qr_code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use clap::Parser;
 use fast_qr::convert::{image::ImageBuilder, svg::SvgBuilder, Builder, Shape};
-use fast_qr::qr::{QRBuilder, QRCodeError};
+use fast_qr::qr::QRBuilder;
+use std::error::Error;
 use std::process::ExitCode;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Create QR-Code
+    /// Create QR-Code from String
     #[arg(short, long)]
     input: String,
 
     /// Save as PNG
-    #[arg(short, long)]
-    file: Option<Option<String>>,
+    #[arg(short, long, value_name = "NAME")]
+    png: Option<Option<String>>,
+
+    /// provide resolution for PNG
+    #[arg(short, long, default_value_t = 600)]
+    resolution: u32,
 
     /// Save as SVG
-    #[arg(short, long)]
+    #[arg(short, long, value_name = "NAME")]
     svg: Option<Option<String>>,
 }
 
@@ -25,25 +30,30 @@ fn save_to_svg(qrcode: &fast_qr::QRCode, file_name: String) {
         .to_file(&qrcode, &file_name);
 }
 
-fn save_to_png(qrcode: &fast_qr::QRCode, file_name: String) {
+fn save_to_png(qrcode: &fast_qr::QRCode, file_name: String, size: u32) {
     let _img = ImageBuilder::default()
         .shape(Shape::Square)
         .background_color([255, 255, 255, 255])
-        .fit_width(600)
+        .fit_width(size)
         .to_file(&qrcode, &file_name);
 }
 
-fn build_qr_code(args: Args) -> Result<(), QRCodeError> {
+fn build_qr_code(args: Args) -> Result<(), Box<dyn Error>> {
     let input = args.input;
     let qrcode = match QRBuilder::new(input).build() {
         Ok(v) => v,
-        Err(e) => return Err(e),
+        Err(e) => return Err(Box::new(e)),
     };
 
-    if args.file.is_some() || args.svg.is_some() {
-        if let Some(file_name) = args.file {
+    if args.png.is_some() || args.svg.is_some() {
+        if let Some(file_name) = args.png {
             let file_name = file_name.unwrap_or(String::from("out.png"));
-            save_to_png(&qrcode, file_name);
+            let size = args.resolution;
+            if size > 10000 {
+                // limiting resolution to prevent high ram usage
+                return Err("resolution too high".into());
+            }
+            save_to_png(&qrcode, file_name, size);
         }
 
         if let Some(file_name) = args.svg {
